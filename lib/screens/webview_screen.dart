676d408b36b7753ea936c8fc2f3aaf26f7fbd2e8
Douglas Gonçalves de Souza <ti.dougg@gmail.com>
2026-08-18 19:49:07 -0300

    Add WebView screen (lib/screens/webview_screen.dart)

---
+++ lib/screens/webview_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://douggs.com.br'));
+  }
+
+  void _onMenuSelected(String value) {
+    if (value == 'fechar') {
+      Navigator.of(context).pop();
+    } else if (value == 'sobre') {
+      showAboutDialog(
+        context: context,
+        applicationName: 'douggs.com.br',
+        applicationVersion: '1.0',
+        children: const [
+          SizedBox(height: 8),
+          Text('Aplicativo que carrega o site douggs.com.br em um WebView.'),
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView'),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: _onMenuSelected,
+            itemBuilder: (context) => const [
+              PopupMenuItem(
+                value: 'fechar',
+                child: Text('Fechar'),
+              ),
+              PopupMenuItem(
+                value: 'sobre',
+                child: Text('Sobre'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
